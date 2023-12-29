82813dee206d0a18dfebd66fa4a67ebfd9679292
Dante Huang <18057332690@sina.cn>
2023-12-29 15:19:13 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # jupiterobot2
-ROS packages for Jupiter2 / Juno2
+ROS packages for Jupiter2 / Juno2 
+
+## Install dependencies
+```
+cd ~/catkin_ws/src
+git clone https://github.com/jupiterobot/jupiterobot2.git
+cd jupiterobot2/
+sudo ./install_pkgs.sh
+cd ~/catkin_ws
+rosdep install --from-paths src --ignore-src -r -y
+```
+## Yolov5 Configuration
+In the home directory
+```
+git clone -b v6.2 https://github.com/ultralytics/yolov5.git
+cd ~/yolov5
+pip3 install -r requirements.txt
+```
+## XFei Lib Configuration
+```
+roscd jupiterobot2_voice_xf && cd lib/
+sudo cp libmsc.so /usr/lib
+```
+## Build the workspace
+```
+cd ~/catkin_ws
+catkin_make
+```
---
+++ install_pkgs.sh
@@ -0,0 +1,33 @@
+sudo apt -y install sox
+
+sudo apt -y install libsox-fmt-all
+
+sudo apt -y install libgoogle-glog-dev
+
+sudo apt -y install ros-noetic-rplidar-ros
+
+sudo apt -y install ros-noetic-slam-gmapping
+
+sudo apt -y install ros-noetic-dwa-local-planner
+
+sudo apt -y install ros-noetic-robotis-manipulator
+
+sudo apt -y install ros-noetic-dynamixel-workbench-toolbox
+
+sudo apt -y install ros-noetic-usb-cam
+
+sudo apt -y install ros-noetic-joint*
+
+sudo apt -y install ros-noetic-libuvc*
+
+sudo aptitude -y install ros-noetic-moveit
+
+sudo aptitude -y install gstreamer1.0-pocketsphinx
+
+sudo aptitude -y install xterm
+
+pip3 install websocket-client pydub pygame
+
+pip3 install mediapipe==0.10.5
+
+pip3 install opencv-contrib-python==4.5.5.64
